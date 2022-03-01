2f9222a0099c0451567c41f4397f08a1c7aebf2d
Sampad Pradhan <sampad.pradhan@girmiti.com>
2022-03-02 02:10:16 +0530

    First commit

--- Practice/src/JAVA_Practice/ClassAccessDifferent.java
+++ Practice/src/JAVA_Practice/ClassAccessDifferent.java
@@ -3,7 +3,7 @@ package JAVA_Practice;
 public class ClassAccessDifferent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
 	}
 
 }
